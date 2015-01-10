52b0205286d3f5664133670efbbfe1778c6c5430
Galen Scovell <galen.scovell@gmail.com>
2015-01-10 12:58:15 -0800

    Introduce new functionality
    Learning Pandas basics, building up ways to parse data

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 This repository is dedicated to practicing parsing of large datasets in an effective and efficient manner.
 
-THe primary program utilizes Pandas to handle the CSV data. I intend to incorporate GUI creation via PyQT and statistical representations through Matplotlib. The end goal is a multifaceted interface for combing through sets of data with visuals describing terms of interest.
+The primary program utilizes Pandas to handle the CSV data. I intend to incorporate GUI creation via PyQT and statistical representations through Matplotlib. The end goal is a multifaceted interface for combing through sets of data with visuals describing terms of interest.
--- parser.py
+++ parser.py
@@ -1,3 +1,13 @@
+'''
+Functions to implement:
+1. Find composition of columns (function, location) as percentages
+    a. Create data representations
+2. Find individual row by search term
+3. Compare two rows next to one another by two search terms
+4. Output results to file for later viewing with date/time
+'''
+
+
 
 import sys
 import pandas as pd
@@ -13,9 +23,32 @@ def termPrompt():
     return term
 
 def fileParse(f, searchTerm):
-    datafile = pd.read_csv(f, header=0)
-    print(datafile[datafile['gene_name'] == searchTerm]) # Specific row
-    print(datafile['function']) # Specific column
+    df = pd.read_csv(f, header=0)
+
+    # Scan for unique elements in column, return percentage out of total
+    function_list = []
+    function_unique = []
+    for row in df.function:
+        if row not in function_list:
+            function_unique.append(row)
+        function_list.append(row)
+
+    for element in function_unique:
+        print(element, (function_list.count(element) / len(function_list)) * 100)
+    
+
+    # All rows with term in column
+    print(df[df.function == searchTerm])
+    # All data under specific column
+    print(df.function)
+    # All column headers
+    print(df.columns)
+    # All data from specific number of rows
+    print(df[:2])
+    # Search individual element
+    for element in df.gene_name:
+        if element == searchTerm:
+            print(element)
 
 
 
