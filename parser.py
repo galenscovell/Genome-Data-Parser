fc16ef5c610d05c69c25f4a9df6323f369aeeb0f
Galen Scovell <galen.scovell@gmail.com>
2015-01-10 17:01:36 -0800

    Excel support, file checking, branching

--- parser.py
+++ parser.py
@@ -9,54 +9,85 @@ Functions to implement:
 
 
 
-import sys
+import sys, os.path, xlrd
 import pandas as pd
 import matplotlib.pyplot as plt
 
 
 def filePrompt():
     file_path = input("Enter the file path ('path/to/file.csv'): ")
-    return file_path
+    if os.path.exists(file_path):
+        return file_path
+    else:
+        print("Unable to locate file.\n")
+        sys.exit()
 
 def termPrompt():
     term = input("Enter the term to return info on: ")
     return term
 
+def scanColumn(df):
+    # Scan for unique elements in column, return percentage out of total
+    print("\nEnter the column header to return info on.")
+    columnName = input(" > ")
+    column_list = []
+    column_unique = []
+    for row in df[columnName]:
+        if row not in column_list:
+            column_unique.append(row)
+        column_list.append(row)
+    print("\n---------------------")
+    for element in column_unique:
+        percentage = column_list.count(element) / len(column_list) * 100
+        print(element + ": %.2f %%" % percentage)
+    print("---------------------")
+
 def fileParse(f, searchTerm):
     df = pd.read_csv(f, header=0)
+    scanColumn(df)
 
-    # Scan for unique elements in column, return percentage out of total
-    function_list = []
-    function_unique = []
-    for row in df.function:
-        if row not in function_list:
-            function_unique.append(row)
-        function_list.append(row)
-
-    for element in function_unique:
-        print(element, (function_list.count(element) / len(function_list)) * 100)
-    
-
-    # All rows with term in column
-    print(df[df.function == searchTerm])
-    # All data under specific column
-    print(df.function)
     # All column headers
-    print(df.columns)
-    # All data from specific number of rows
-    print(df[:2])
+    #print(df.columns)
+    # All rows with term in column
+    #print(df[df.function == searchTerm])
+    # All data under specific column
+    #print(df.function)
+   # All data from specific number of rows
+    #print(df[:2])
     # Search individual element
-    for element in df.gene_name:
-        if element == searchTerm:
-            print(element)
+    #for element in df.gene_name:
+        #if element == searchTerm:
+            #print(element)
 
 
 
 def main():
-    print("\nPython CSV Parser loaded.")
     f = filePrompt()
-    t = termPrompt()
-    fileParse(f, t)
+
+    print("\n###### Python Data Parser loaded ######\n")
+    if f.endswith('.csv'):
+        dataframe = pd.read_csv(f, header=0)
+    elif f.endswith('.xls') or f.endswith('.xlsx'):
+        dataframe = pd.read_excel(f, header=0)
+    else:
+        print("File extension needs to be .csv, .xls, or .xlsx")
+        print("\n###### Python Data Parser finished ###### \n")
+        sys.exit()
+
+    print("What would you like to do with the data?")
+    print("(Options: Scan [C]olumns, Search [R]ows, [E]xit)")
+    choice = ""
+    while choice != 'c' and choice != 'r':
+        choice = input(" > ")
+        if choice[0].lower() == 'c':
+            scanColumn(dataframe)
+        elif choice[0].lower() == 'r':
+            t = termPrompt()
+            fileParse(f, t)
+        else:
+            break
+
+    print("\n###### Python Data Parser finished ###### \n")
 
 
 if __name__ == '__main__':
