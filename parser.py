9f3e4439239d7f312f2d7b4d3813f02d91677e6b
Galen Scovell <galen.scovell@gmail.com>
2015-01-14 21:14:25 -0800

    Begin Keyword searching, refine filters

--- parser.py
+++ parser.py
@@ -20,32 +20,18 @@ import matplotlib.pyplot as plt
 
 
 
+
 def filePrompt():
     # Ask for file path, check that file exists
     file_path = input("Enter the file path ('path/to/file.csv'): ")
     if os.path.exists(file_path):
         return file_path
     else:
-        print("Unable to locate file.\n")
+        print("\tUnable to locate file.\n")
         sys.exit()
 
 
-def scanColumn(df):
-    # Scan for unique elements in column
-    column_entry = " "
-    header_info = list(df)
-    while column_entry not in header_info:
-        print("\nFollowing column headers found:")
-        print(header_info)
-        print("\nEnter the column header to return info on")
-        column_entry = input(" > ")
-    column_list = []
-    column_unique = []
-    for row in df[column_entry]:
-        if row not in column_list:
-            column_unique.append(row)
-        column_list.append(row)
-    
+def createGraph(analyzed, total):
     # Pie-chart output init
     labels = []
     sizes = []
@@ -53,16 +39,16 @@ def scanColumn(df):
     explode = []
 
     # Create 'explode' pie piece effect
-    for n in column_unique:
+    for n in analyzed:
         explode.append(0.05)
 
     # Fill pie-chart output with ratios of column of interest
-    for element in column_unique:
-        percentage = column_list.count(element) / len(column_list) * 100
+    for element in analyzed:
+        percentage = total.count(element) / len(total) * 100
         labels.append(element)
         sizes.append(percentage)
 
-    # Show completed pie-chart
+    # Display completed pie-chart
     plt.rcParams['font.size'] = 9.0
     plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.2f%%', shadow=True, startangle=90)
     plt.axis('equal')
@@ -70,18 +56,67 @@ def scanColumn(df):
     print("\n---------------------")
 
 
+#def refine
+
+
+def scanColumn(df):
+    # Scan for all unique elements in column
+    column_choice = " "
+    header_info = list(df)
+    while column_choice not in header_info:
+        print("\nFollowing column headers found:")
+        print(header_info)
+        print("\n\tEnter column to return info on")
+        column_choice = input("\t > ")
+    column_total = []
+    column_unique = []
+    for row in df[column_choice]:
+        if row not in column_total:
+            column_unique.append(row)
+        column_total.append(row)
+
+    filter_decision = " "
+    while filter_decision[0].lower() not in ('d', 'r'):
+        print("\n\tAnalyze this data or further refine?")
+        print("\t(Options: [D]one or [R]efine)")
+        filter_decision = input("\t > ")
+        if filter_decision[0].lower() == 'd':
+            break
+        else:
+            print("\tRefinement here.")
+
+    graph_choice = " "
+    while graph_choice[0].lower() not in ('y', 'n'):
+        print("\n\tCreate pie-chart with collected data (Y/N)?")
+        graph_choice = input("\t > ")
+        if graph_choice[0].lower() == 'y':
+            createGraph(column_unique, column_total)
+        else:
+            break
+
+
 def termPrompt():
     # Ask for search term of interest
-    print("\nEnter the term to return info on")
-    term = input(" > ")
+    print("\n\tEnter search term")
+    term = input("\t > ")
+    print("")
     return term
 
 
-def searchRow(df, search):
+def searchKeywords(df, search):
     # Return rows with term anywhere in columns
-    for column in df.columns:
-        if len(df[df[column] == search]) > 0:
-            print(df[df[column] == search])
+    # for column in df.columns:
+    #     if len(df[df[column] == search]) > 0:
+    #         print(df[df[column] == search])
+    #     else:
+    #         print("No results found for '" + search + "' in '" + column + "'")
+    
+    print(len(df))
+    for row in df.Keywords:
+        if type(row) is str and ';' in row:
+                row_subarray = row.split(';')
+                if search in row_subarray:
+                    print(df[row].iloc)
     print("\n---------------------")
     
     
@@ -96,21 +131,21 @@ def main():
     elif f.endswith('.xls') or f.endswith('.xlsx'):
         dataframe = pd.read_excel(f, header=0)
     else:
-        print("File extension needs to be .csv, .xls, or .xlsx")
+        print("\tFile extension needs to be .csv, .xls, or .xlsx")
         sys.exit()
 
     running = True
     while running:
         choice = " "
-        print("\nFILE IN USE: " + f)
-        print("(Options: Scan [C]olumns, Search [R]ows, [E]xit)")
-        while choice[0].lower() != 'c' and choice[0].lower() != 'r':
+        print("\nFILE IN USE: " + f + " (" + str(len(dataframe)) + " rows)")
+        print("(Options: Scan [C]olumns, Search [K]eyword, [E]xit)")
+        while choice[0].lower() not in ('c', 'k', 'e'):
             choice = input(" > ")
             if choice[0].lower() == 'c':
                 scanColumn(dataframe)
-            elif choice[0].lower() == 'r':
+            elif choice[0].lower() == 'k':
                 t = termPrompt()
-                searchRow(dataframe, t)
+                searchKeywords(dataframe, t)
             elif choice[0].lower() == 'e':
                 running = False
                 break
