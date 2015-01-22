61be321f43db2bacc2b6882b5bd8ea8f1ed2e987
Galen Scovell <galen.scovell@gmail.com>
2015-01-22 11:29:26 -0800

    Begin tkinter object-oriented organization

--- interface.py
+++ interface.py
@@ -1,39 +1,57 @@
 
 from tkinter import *
 from tkinter import ttk
+from tkinter.filedialog import askopenfilename
 
 
-# Initial window setup
-root = Tk()
-root.title("Genome Data Parser")
+class OpenFileWindow():
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Genome Data Parser")
+
+        # Widgets
+        self.frame = ttk.Frame(self.root, padding=20)
+        self.label = ttk.Label(self.frame, anchor=CENTER, text="Choose the file to open:")
+        self.entry = Text(self.frame, width=40, height=2)
+        self.exit_button = ttk.Button(self.frame, text='Exit', command='', width=16)
+        self.exit_button.state(['disabled'])
+        self.open_button = ttk.Button(self.frame, text='Open', command='', width=16)
+        # browser = askopenfilename(parent=root)
+
+        # Layout
+        self.frame.grid(column=0, row=0, sticky=(N, W, E, S))
+        self.label.grid(column=1, row=1)
+        self.entry.grid(column=1, row=2)
+        self.open_button.grid(column=1, row=3, sticky=W)
+        self.exit_button.grid(column=1, row=3, sticky=E)
+
+        # Grid Configuration
+        self.root.columnconfigure(0, weight=1)
+        self.root.rowconfigure(0, weight=1)
+        self.frame.columnconfigure(0, weight=3)
+        self.frame.columnconfigure(1, weight=3)
+        self.frame.columnconfigure(2, weight=3)
+        self.frame.rowconfigure(0, weight=3)
+        self.frame.rowconfigure(1, weight=3)
+        self.frame.rowconfigure(2, weight=3, pad=6)
+        self.frame.rowconfigure(3, weight=3, pad=6)
+
+    def close_window(self):
+        print("Nothin' yet")
 
 
-# Main content frame, height/width determined by inner content
-content_frame = ttk.Frame(root, padding=20)
-content_frame.grid(column=0, row=0, sticky=(N, W, E, S))
-content_frame.columnconfigure(0, weight=1)
-content_frame.rowconfigure(0, weight=1)
+class MainWindow():
+    def __init__(self, root):
+        self.root = root
 
 
-# Label creation
-content_variable = StringVar()
-ex_label = ttk.Label(content_frame, anchor=CENTER, textvariable=content_variable)
-content_variable.set("Enter path/to/file.csv")
-ex_label.pack()
 
 
-# Entry creation
-file_entry = StringVar()
-file_path_entry = ttk.Entry(content_frame, textvariable=file_entry, width=50)
-file_path_entry.pack()
+def main():
+    root = Tk()
+    app = OpenFileWindow(root)
+    root.mainloop()
 
 
-# Button creation
-exit_button = ttk.Button(content_frame, text='Exit', command='')
-exit_button.state(['disabled'])
-exit_button.pack(side=RIGHT, padx=8, pady=8)
-open_button = ttk.Button(content_frame, text='Open', command='')
-open_button.pack(side=RIGHT)
-
-
-root.mainloop()
+if __name__ == '__main__':
+    main()
