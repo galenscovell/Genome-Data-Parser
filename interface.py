5568e38b28679b8763d61ffa6b3cbd485656698a
Galen Scovell <galen.scovell@gmail.com>
2015-01-25 12:26:43 -0800

    User input to console, text formatting tags

--- interface.py
+++ interface.py
@@ -3,7 +3,7 @@ from cmd_parser import DataParser
 from tkinter import *
 from tkinter import ttk, font, messagebox
 from tkinter.filedialog import askopenfilename
-import time, os.path
+import sys, os.path, time
 
 
 class MainWindow():
@@ -21,26 +21,26 @@ class MainWindow():
         self.console_frame = ttk.Frame(self.main_frame)
         self.console_frame['borderwidth'] = 20
         self.console_frame['relief'] = 'solid'
-        self.button_frame = ttk.Frame(self.main_frame)
+        self.btn_frame = ttk.Frame(self.main_frame)
 
 
-        self.console_label = ttk.Label(self.console_frame, text="Console Output:")
+        self.console_label = ttk.Label(self.console_frame, text='Console Output:')
         self.console_text = Text(self.console_frame, width=70, height=20, font=self.custom_font)
         self.console_scrollbar = ttk.Scrollbar(self.console_frame, command=self.console_text.yview)
         self.console_text.config(yscrollcommand=self.console_scrollbar.set)
-        self.input_label = StringVar()
-        self.console_input_label = ttk.Label(self.console_frame, textvariable=self.input_label)
-        self.console_input = ttk.Entry(self.console_frame, width=50, font=self.custom_font)
-        self.update_input_label('User Input:', 'disabled')
-        self.console_text.insert(END, "[ Parser Ready ]" + (" " * 44) + time.strftime("%d/%m/%Y"))
-        self.console_text.insert(END, "\n" + "-" * 70)
-        self.console_text.insert(END, "\nWaiting for csv/xls/xlsx file...")
+        self.user_input_label = ttk.Label(self.console_frame, text='User Input:')
+        self.user_input = ttk.Entry(self.console_frame, width=50, font=self.custom_font)
+        self.console_text.insert(END, 'Transcriptome Data Parser' + (' ' * 35) + time.strftime('%d/%m/%Y'))
+        self.console_text.insert(END, '\n' + ('-' * 70))
+        self.console_text.insert(END, '\nWaiting for csv/xls/xlsx file...')
         self.console_text.config(state=DISABLED)
+        self.console_text.tag_configure('green', foreground='green')
+        self.console_text.tag_configure('blue', foreground='blue')
 
 
-        self.exit_button = ttk.Button(self.button_frame, width=18, text='Exit', command=self.close_window)
-        self.open_button = ttk.Button(self.button_frame, width=18, text='Open', command=self.file_browser)
-        self.save_button = ttk.Button(self.button_frame, width=18, text='Save', command=self.save_output)
+        self.exit_btn = ttk.Button(self.btn_frame, width=18, text='Exit', command=self.close_window)
+        self.open_btn = ttk.Button(self.btn_frame, width=18, text='Open', command=self.file_browser)
+        self.save_btn = ttk.Button(self.btn_frame, width=18, text='Save', command=self.save_output)
 
 
 
@@ -49,19 +49,19 @@ class MainWindow():
         # -------------------------------------------------- #
         self.main_frame.grid(column=0, row=0, sticky=(N, W, E, S))
         self.console_frame.grid(column=0, row=0, sticky=N)
-        self.button_frame.grid(column=0, row=3, columnspan=3, sticky=S)
+        self.btn_frame.grid(column=0, row=3, columnspan=3, sticky=S)
 
 
         self.console_label.grid(column=0, row=0, sticky=W)
         self.console_text.grid(column=0, row=1)
         self.console_scrollbar.grid(column=1, row=1, sticky=NS)
-        self.console_input_label.grid(column=0, row=2, pady=(20, 0), sticky=W)
-        self.console_input.grid(column=0, row=2, pady=(20, 0))
+        self.user_input_label.grid(column=0, row=2, pady=(20, 0), sticky=W)
+        self.user_input.grid(column=0, row=2, pady=(20, 0))
 
 
-        self.exit_button.grid(column=2, row=0, sticky=E)
-        self.open_button.grid(column=1, row=0, padx=(20, 20))
-        self.save_button.grid(column=0, row=0, sticky=W)
+        self.exit_btn.grid(column=2, row=0, sticky=E)
+        self.open_btn.grid(column=1, row=0, padx=(20, 20))
+        self.save_btn.grid(column=0, row=0, sticky=W)
 
 
 
@@ -81,53 +81,69 @@ class MainWindow():
         self.console_frame.rowconfigure(1, weight=3)
         self.console_frame.rowconfigure(2, weight=3)
 
-        self.button_frame.columnconfigure(0, weight=3)
-        self.button_frame.columnconfigure(1, weight=3)
-        self.button_frame.columnconfigure(2, weight=3)
-        self.button_frame.rowconfigure(0, weight=3)
+        self.btn_frame.columnconfigure(0, weight=3)
+        self.btn_frame.columnconfigure(1, weight=3)
+        self.btn_frame.columnconfigure(2, weight=3)
+        self.btn_frame.rowconfigure(0, weight=3)
 
 
 
         # -------------------------------------------------- #
         # -                  Key Bindings                  - #
         # -------------------------------------------------- #
-        # root.bind('<Return>', lambda x: # User input to console)
-        root.bind('<Escape>', self.close_window)
-        root.protocol('WM_DELETE_WINDOW', self.close_window)
+        self.user_input.bind('<Return>', self.user_to_console)
+        self.root.bind('<Escape>', self.close_window)
+        self.root.protocol('WM_DELETE_WINDOW', self.close_window)
 
 
 
-    def update_input_label(self, textvar, state):
-        self.input_label.set(textvar)
-        self.console_input.state([state])
-
-    def update_option_label(self, textvar):
-        self.option_text.set(textvar)
-
-    def close_window(self, event=0):
+    def close_window(self, event=None):
         if messagebox.askyesno(message='Are you sure you want to quit? Any unsaved results will be lost.', title='Close Parser', icon='info'):
             self.root.destroy()
-
-    def file_browser(self):
-        data_file = askopenfilename(parent=self.root, filetypes=(("CSV files", "*.csv"),("Excel files", "*.xls;*.xlsx")))
-        allowed_types = ('.csv', '.xls', '.xlsx')
-        if data_file.endswith(allowed_types):
-            self.parser.check_file(data_file)
-            load_message = "File loaded: " + os.path.basename(data_file)
-            self.update_console(load_message)
-        elif not data_file:
-            pass
-        else:
-            self.update_console("File extension must be .csv, .xls, or .xlsx")
+            sys.exit()
 
     def save_output(self):
         print("Nothin' yet!")
 
-    def update_console(self, output):
+    def user_to_console(self, event=None):
+        if self.user_input.get():
+            message = ' > ' + self.user_input.get()
+            self.update_console(message, 'blue')
+
+    def update_console(self, output, tag=None):
         self.console_text.config(state=NORMAL)
-        self.console_text.insert(END, "\n" + output)
-        self.console_input.delete(0, END)
+        self.console_text.insert(END, "\n" + output, tag)
+        self.user_input.delete(0, END)
         self.console_text.config(state=DISABLED)
+
+    def file_browser(self):
+        datafile = askopenfilename(parent=self.root, filetypes=(("CSV files", "*.csv"),("Excel files", "*.xls;*.xlsx")))
+        allowed_types = ('.csv', '.xls', '.xlsx')
+        if datafile.endswith(allowed_types):
+            dataframe = self.parser.check_file(datafile)
+            load_message = "\nFile loaded: " + os.path.basename(datafile) + " (" + str(len(dataframe)) + " rows in file)"
+            self.update_console(load_message)
+            self.program_main(dataframe)
+        elif not datafile:
+            pass
+        else:
+            self.update_con("File extension must be .csv, .xls, or .xlsx")
+
+    def program_main(self, df):
+        running = True
+        while running:
+            choice = " "
+            self.update_console("Options: [C]olumn Composition, [K]eyword Search", 'green')
+            while len(choice) == 0 or choice[0].lower() not in ('c', 'k'):
+                choice = input(" > ")
+                if len(choice) > 0:
+                    if choice[0].lower() == 'c':
+                        column = self.parser.pick_column(df)
+                        self.parser.scan_column(df, column)
+                    elif choice[0].lower() == 'k':
+                        column = self.parser.pick_column(df)
+                        term = self.parser.term_prompt()
+                        self.parser.search_keyword(df, column, term, len(df))
 
 
 
