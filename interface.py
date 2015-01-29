97777a2a67dd6e4cb92468884dee59d20319d3d8
Galen Scovell <galen.scovell@gmail.com>
2015-01-29 12:40:11 -0800

    User keyword search entry, update organization

--- interface.py
+++ interface.py
@@ -3,9 +3,6 @@ import sys, os.path, time, xlrd
 
 import pandas as pd
 from pandas import DataFrame
-
-import matplotlib
-matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
 from tkinter import *
@@ -28,16 +25,17 @@ class MainWindow():
         self.console_frame['relief'] = 'groove'
         self.btn_frame = ttk.Frame(self.main_frame)
 
-
         self.console_label = ttk.Label(self.console_frame, text='Console Output:')
+        self.current_file = StringVar()
+        self.current_label = ttk.Label(self.console_frame, textvariable=self.current_file)
+        self.current_file.set('No file loaded.')
         self.console_text = Text(self.console_frame, width=70, height=20, font=self.custom_font, wrap=WORD)
         self.console_scrollbar = ttk.Scrollbar(self.console_frame, command=self.console_text.yview)
         self.console_text.config(yscrollcommand=self.console_scrollbar.set)
         self.console_text.insert(END, 'Transcriptome Data Parser' + (' ' * 35) + time.strftime('%d/%m/%Y'))
 
-        self.current_file = StringVar()
-        self.current_label = ttk.Label(self.console_frame, textvariable=self.current_file)
-        self.current_file.set('No file loaded.')
+        self.search_input = ttk.Entry(self.console_frame, width=40)
+        self.search_input.config(state=DISABLED)
         self.options_list = StringVar()
         self.options_box = Listbox(self.console_frame, height=8, width=50, listvariable=self.options_list, activestyle='none', selectbackground='#2ecc71', font=self.custom_font)
         self.options_scrollbar = ttk.Scrollbar(self.console_frame, command=self.options_box.yview)
@@ -49,11 +47,9 @@ class MainWindow():
         self.console_text.tag_configure('green', foreground='green')
         self.console_text.tag_configure('blue', foreground='blue')
 
-
         self.exit_btn = ttk.Button(self.btn_frame, width=18, text='Exit', command=self.close_window)
         self.open_btn = ttk.Button(self.btn_frame, width=18, text='Open', command=self.file_browser)
         self.save_btn = ttk.Button(self.btn_frame, width=18, text='Save', command=self.save_output)
-
 
 
         # -------------------------------------------------- #
@@ -65,16 +61,16 @@ class MainWindow():
 
 
         self.console_label.grid(column=0, row=0, sticky=W)
+        self.current_label.grid(column=0, row=0, sticky=E, ipady=10)
         self.console_text.grid(column=0, row=1)
         self.console_scrollbar.grid(column=1, row=1, sticky=NS)
-        self.current_label.grid(column=0, row=0, sticky=E, ipady=20)
-        self.options_box.grid(column=0, row=3, columnspan=2, pady=(10, 0))
+        self.search_input.grid(column=0, row=2, pady=4, ipady=10, columnspan=2)
+        self.options_box.grid(column=0, row=3, columnspan=2)
         self.options_scrollbar.grid(column=1, row=3, sticky=NS)
 
         self.exit_btn.grid(column=2, row=0, sticky=E)
         self.open_btn.grid(column=1, row=0, padx=(20, 20))
         self.save_btn.grid(column=0, row=0, sticky=W)
-
 
 
         # -------------------------------------------------- #
@@ -100,15 +96,20 @@ class MainWindow():
         self.btn_frame.rowconfigure(0, weight=3)
 
 
-
         # -------------------------------------------------- #
         # -                  Key Bindings                  - #
         # -------------------------------------------------- #
         self.root.protocol('WM_DELETE_WINDOW', self.close_window)
         self.options_box.bind('<Double-Button-1>', self.select_list_item)
         self.options_box.bind('<Return>', self.select_list_item)
+        self.search_input.bind('<Return>', self.get_search_input)
         
 
+
+
+    # -------------------------------------------------- #
+    # -                GUI Functions                   - #
+    # -------------------------------------------------- #
     def close_window(self):
         if messagebox.askyesno(message='Are you sure you want to quit? Any unsaved results will be lost.', title='Close Parser', icon='info'):
             self.root.destroy()
@@ -130,16 +131,27 @@ class MainWindow():
         self.update_console(' > ' + value)
         if self.state == 'beginning':
             if value == 'Scan Column Composition':
+                self.state = 'column_for_scan'
                 self.pick_column(self.dataframe)
             elif value == 'Keyword Search':
+                self.state = 'column_for_keyword'
                 self.pick_column(self.dataframe)
                 # self.parser.search_keyword(self.dataframe, column, term, len(self.dataframe), self)
-        elif self.state == 'choosing_column':
+        else:
             self.column_choice = value
             self.options_list.set('')
-            self.scan_column(self.dataframe)
-            self.program_begin()
+            if self.state == 'column_for_scan':
+                self.scan_column(self.dataframe)
+                self.program_begin()
+            elif self.state == 'column_for_keyword':
+                self.term_prompt()
 
+    def get_search_input(self, event):
+        self.search_term = self.search_input.get()
+        self.search_input.delete(0, END)
+        self.search_input.config(state=DISABLED)
+        self.update_console(' > ' + self.search_term)
+            
     def file_browser(self):
         self.data_file = filedialog.askopenfilename(parent=self.root, filetypes=(('CSV files', '*.csv'),('Excel files', '*.xls;*.xlsx')))
         allowed_types = ('.csv', '.xls', '.xlsx')
@@ -180,11 +192,12 @@ class MainWindow():
         self.update_console(header_message, 'blue')
         self.update_console('[Select Column of interest]', 'green')
         self.options_list.set(tuple(header_info))
-        self.state = 'choosing_column'
 
     def term_prompt(self):
         # Ask for search term of interest
-        self.update_console('\n\tEnter search term (case-sensitive):', 'green')
+        self.update_console('\n[Enter search term below (case-sensitive)]', 'green')
+        self.search_input.config(state=NORMAL)
+        self.search_input.focus()
 
     def scan_column(self, df):
         # Scan for all unique elements in column
@@ -221,6 +234,9 @@ class MainWindow():
 
 
 
+
+
+
 def create_interface():
     root = Tk()
     
@@ -230,7 +246,7 @@ def create_interface():
 
     screen_x = root.winfo_screenwidth()
     screen_y = root.winfo_screenheight()
-    window_x, window_y = 600, 480
+    window_x, window_y = 600, 540
     x_pos = (screen_x / 2) - (window_x / 2)
     y_pos = (screen_y / 2) - (window_y / 2)
     root.geometry('%dx%d+%d+%d' % (window_x, window_y, x_pos, y_pos))
